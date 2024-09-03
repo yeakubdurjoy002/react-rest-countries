243c0f4f554a76082f1c595ded1441ea91c2051f
YeakubDurjoy001 <yeakubdurjoy002>
2024-09-03 23:17:29 +0600

    added some important elements

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
-  const { name, flags } = country;
+  const { name, flags, population, area, cca3 } = country;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisiting = () => {
+    setVisited(!visited);
+  };
   return (
     <div className="country">
       <h3>Country Name : {name?.common}</h3>
       <img src={flags.png} alt="" />
+      <p>Population : {population}</p>
+      <p>Area : {area}</p>
+      <p>Code : {cca3}</p>
+      <button onClick={handleVisiting}>Click to visit</button>
+      {visited
+        ? "I have visited this country"
+        : "I want  to visit this country"}
     </div>
   );
 };
